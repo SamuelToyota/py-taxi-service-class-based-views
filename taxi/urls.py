602d9b2f3b72ec44eed42f76f13baa5102d905c2
Samuel Yudi <toyotaa038@gmail.com>
2025-11-04 10:24:56 -0300

    poletao

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from . import views
 
-from django.urls import path
-from . import views  # ← duplicado
-
 app_name = "taxi"
 
 urlpatterns = [
     path("manufacturers/", views.ManufacturerListView.as_view(), name="manufacturer-list"),
-    ...
-]
-
-urlpatterns = [  # ← duplicado
-    ...
+    path("cars/", views.CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", views.CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", views.DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/", views.DriverDetailView.as_view(), name="driver-detail"),
 ]
